8b2444f040567308988d490d6b17024403e1b8a2
leen-neel <and24903@gmail.com>
2023-07-12 15:47:52 +0530

    added basehead

---
+++ src/config.ts
@@ -0,0 +1,3 @@
+export const SITE_TITLE = "Anindo Neel Dutta | Full-Stack Web Developer";
+export const DESCRIPTION =
+  "Hey there! I am Anindo Neel Dutta! I hail from India. I'm a full-stack web developer who makes web apps with the MEVN stack";
