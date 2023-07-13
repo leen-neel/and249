009517560eeb04863e889b3c7665e616982cdac2
leen-neel <and24903@gmail.com>
2023-07-13 22:01:45 +0530

    added color themes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,4 +14,29 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#26A69A",
+
+          secondary: "#d926a9",
+
+          accent: "#1fb2a6",
+
+          neutral: "#2a323c",
+
+          "base-100": "#041E1A",
+
+          info: "#3abff8",
+
+          success: "#36d399",
+
+          warning: "#fbbd23",
+
+          error: "#f87272",
+        },
+      },
+    ],
+  },
 };
