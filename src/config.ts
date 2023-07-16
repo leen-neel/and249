0665d661d6e7ac45f0c43c3aa48abbeb5bfb1769
leen-neel <and24903@gmail.com>
2023-07-16 19:52:12 +0530

    added proper prosp to layout

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
-export const SITE_TITLE = "Anindo Neel Dutta | Full-Stack Web Developer";
-export const DESCRIPTION =
+export const SITE_TITLE = "Anindo Neel Dutta - Full-Stack Web Developer";
+export const SITE_DESC =
   "Hey there! I am Anindo Neel Dutta! I hail from India. I'm a full-stack web developer who makes web apps with the MEVN stack";
+export const SITE_IMG = "https://and249.codes/images/Myself.";
