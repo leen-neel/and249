7a55541cdb227cbe0de185a19b9807c4632c5c28
leen-neel <and24903@gmail.com>
2023-08-15 15:31:39 +0530

    added build script

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+FILE=./site.zip
+if test -f "$FILE"; then
+    echo "removing old build file"
+    rm ./site.zip
+fi
+
+npm run build
+cd ./dist
+zip -r ../site.zip ./*
+
+# figlet "Site built!"
+# sleep 3
+# clear
