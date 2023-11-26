0c563ebbc0ad320b011be3ce59d3724d7356ca07
leen-neel <and24903@gmail.com>
2023-11-26 09:15:28 +0530

    changed data file names

---
+++ src/data/skills.ts
@@ -0,0 +1,28 @@
+const skills = [
+  {
+    name: "Vue",
+    icon: "vue",
+  },
+  {
+    name: "Quasar",
+    icon: "simple-icons:quasar",
+  },
+  {
+    name: "Astro",
+    icon: "astro",
+  },
+  {
+    name: "TypeScript",
+    icon: "vscode-icons:file-type-typescript-official",
+  },
+  {
+    name: "WordPress",
+    icon: "bi:wordpress",
+  },
+  {
+    name: "NodeJS",
+    icon: "logos:nodejs-icon",
+  },
+];
+
+export default skills;
---
+++ src/data/socials.ts
@@ -0,0 +1,24 @@
+const socials = [
+  {
+    link: "https://github.com/leen-neel",
+    icon: "tabler:brand-github",
+    name: "GitHub",
+  },
+  {
+    link: "https://t.me/and_249/",
+    icon: "tabler:brand-telegram",
+    name: "Telegram",
+  },
+  {
+    link: "https://www.instagram.com/and_249/",
+    icon: "tabler:brand-instagram",
+    name: "Instagram",
+  },
+  {
+    link: "https://www.linkedin.com/in/and249/",
+    icon: "tabler:brand-linkedin",
+    name: "LinkedIn",
+  },
+];
+
+export default socials;
