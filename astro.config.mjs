1d39c3a76a8b0227477dd3531a978bf226035078
leen-neel <and24903@gmail.com>
2023-12-09 08:52:59 +0530

    added vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 import vue from "@astrojs/vue";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap(), vue()],
   site: "https://and249.codes/",
   output: "server",
+  adapter: vercel()
 });
