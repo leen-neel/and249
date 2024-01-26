4bf303ee7ee58dc5beda9e4e523f929f03cb0419
leen-neel <and24903@gmail.com>
2024-01-26 10:28:19 +0530

    updated og image

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@ export const SITE_TITLE =
   "Anindo Neel Dutta - Web Artisan Building the Interwebs";
 export const SITE_DESC =
   "Hey there! I am Anindo Neel Dutta! I hail from India. I'm a full-stack web developer who makes web apps with the MEVN stack";
-export const SITE_IMG = "https://images.and249.codes/portfolio/OG.png";
+export const SITE_IMG = "https://anindoneel.com/OG.png";
