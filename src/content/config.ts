45c3b20a4ded7d654002e71ffb3ff76d2492fd06
leen-neel <and24903@gmail.com>
2024-02-26 09:30:52 +0530

    added blog

---
+++ src/content/config.ts
@@ -0,0 +1,18 @@
+import { defineCollection, z } from "astro:content";
+
+const blogCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.string(),
+    heroImage: z.string(),
+    category: z.string(),
+    tags: z.array(z.string()),
+    draft: z.boolean(),
+  }),
+});
+
+export const collections = {
+  blog: blogCollection,
+};
