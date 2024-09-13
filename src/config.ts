534bf1f5481a1291acf55c15e39845ee49d4fbe7
leen-neel <and24903@gmail.com>
2024-09-13 11:55:14 +0530

    fixed canonical tags

--- src/config.ts
+++ src/config.ts
@@ -3,3 +3,4 @@ export const SITE_TITLE =
 export const SITE_DESC =
   "Hey there! I am Anindo Neel Dutta! I hail from India. I'm a full-stack web developer who makes web apps with the MEVN stack";
 export const SITE_IMG = "https://anindoneel.com/OG.png";
+export const SITE_URL = "https://anindoneel.com/";
